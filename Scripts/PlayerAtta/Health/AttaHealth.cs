6619b3a9c0dd5db1e40a3631abdacafdf73f2fd3
majesticsummits <117538035+alympyan@users.noreply.github.com>
2022-11-06 02:57:51 -0500

    Update AttaHealth.cs

--- Scripts/PlayerAtta/Health/AttaHealth.cs
+++ Scripts/PlayerAtta/Health/AttaHealth.cs
@@ -86,6 +86,12 @@ namespace AwesomsseyEngine
             {
                 CurrentPlayerMaxHealth = 10f;
             }
+
+            ///HEALTH MAX LIMIT
+            if(currentPlayerHealth >= CurrentPlayerMaxHealth)
+            {
+                currentPlayerHealth = CurrentPlayerMaxHealth;
+            }
         }
     }
 }
